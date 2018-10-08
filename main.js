0ba2a9ffdde39a53e33bc9b16b5b82b995c52e20
Eric Whited <whited.j.eric@gmail.com>
2018-10-08 11:05:00 -0400

    added win and loose

--- main.js
+++ main.js
@@ -1,49 +1,88 @@
-$(document).ready(function() {
+var hintArea = document.getElementById('hintArea')
+var letterArea = document.getElementById('letterArea')
+var usedLetters = document.getElementById('usedLetters')
+var letters = document.getElementsByClassName('buttons')
+var wordsArray = [];
+var lives
 
-// var incorrect = 0;
-var word = ['T', 'R', 'E', 'E'];
-// var strikes = 0;
-// var incorrect = false;
+class wordBank {
 
-
-
-$('.buttons').click(function() {
-    var found = false;
-    var strikes = 0;
-    for(i=0; i<word.length; i++) {
-        // if the button I push is = to any of the letters in word
-       if($(this).text() == word[i]) {
-           found = true;
-        //    change the letter box to that letter
-            $('.letterBox')[i].innerHTML = $(this).text();
-        }
+    constructor(word, hint) {
+        this.word = word
+        this.hint = hint
+        // adds a word and a hint to the word bank
+        wordsArray.push(this)
     }
-        
-
-
-
-
-
-    
-if(found == false) {
-    $('.chance')[0].style = 'color: red;'
-    strikes = strikes++;
-    console.log(strikes);
 }
 
+// Creating words & hints
+var word1 = new wordBank('phone', 'never leaves my side')
+var word2 = new wordBank('shoes', 'Favorite item to shop for')
+var word3 = new wordBank('dog', 'fav animal')
 
-    
-})
+setTimeout(startGame(),1000)
+
+function startGame() {
+    lives = 3;
+    generateWord();
+
+    function generateWord() {
+        for(i = 0; i < letters.length; i++) {
+            letters[i].disabled = false
+        }
+
+        guessedLetters = [];
+        usedLetters.innerHTML = ""
+        // helps pick a random word
+        randomNum = Math.floor(Math.random() * Math.floor(wordsArray.length));
+        // picking a word out of the array 
+        actualWord = wordsArray[randomNum].word.split("")
+        // maps word into underscores
+        hiddenWord = actualWord.map(function(x){return "_"})
+        // joins area of underscores into a word
+        letterArea.innerHTML = hiddenWord.join(" ")
+        // grabs hint from wordbank
+        hintArea.innerHTML = wordsArray[randomNum].hint
 
 
-    // if(incorrect == true) {
-    //     alert('incorrect');
-    //     strikes = strikes+1
-    //     // console.log(strikes);
-    // }
-    
+    }
 
+    for (let i=0; i < letters.length; i++) {
+        letters[i].addEventListener("click", function(){
+            if(guessedLetters.includes(letters[i].value)){
 
+            }else {
+                if(actualWord.includes(letters[i].value.toLowerCase())) {
+                    for(let j = 0; j < actualWord.length; j++) {
+                        if(actualWord[j] === letters[i].value.toLowerCase()) {
+                            hiddenWord.splice(j,1,letters[i].value);
+                        }
+                    }
+                    letterArea.innerHTML = hiddenWord.join(" ")
 
-})
+                }else {
+                    lives--;
+                    if(lives==0){
+                        lose();
+                    }
+                } 
+                guessedLetters.push(letters[i].value);
+                usedLetters.innerHTML = guessedLetters.join(", ");
+                letters[i].disabled = true;
+            }
+            if(hiddenWord.includes('_')==false){
+                win();
+            }
+        })
+    }
+}
 
+function win() {
+    setTimeout(function(){alert('You winnnnnnn');}, 1000);
+    setTimeout(startGame(), 3000);
+}
+
+function lose() {
+    setTimeout(function(){alert('You suck');}, 1000);
+    setTimeout(startGame(), 3000);
+}
---
+++ second.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+var words = {
+    cat: 'animal'
+}
+
+
+
+})
